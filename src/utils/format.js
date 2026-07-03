cf3fb806c266e1e9a6e587aeafe17de2028191e8
= <tjalswhd1@naver.com>
2026-07-03 13:22:36 +0900

    refactor : 전화번호 형식 로직 리팩토링

--- src/utils/format.js
+++ src/utils/format.js
@@ -12,7 +12,11 @@ export const formatDate = (dateStr) => {
 // 휴대폰 번호 하이픈(-) 추가 포맷
 export const formatPhoneNumber = (phone) => {
     if (!phone) return '정보 없음'
-    // 숫자만 추출한 뒤 포맷팅
-    const cleaned = phone.replace(/[^0-9]/g, '')
-    return cleaned.replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`)
+
+    const cleaned = String(phone).replace(/[^0-9]/g, '')
+
+    return cleaned.replace(
+        /^(\d{2,3})(\d{3,4})(\d{4})$/,
+        '$1-$2-$3'
+    )
 }
